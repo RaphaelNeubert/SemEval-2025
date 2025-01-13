8ef05190c92a1d75633c265cc5918ef0d85eec85
Raphael Neubert <raphi.neubert@gmail.com>
2025-01-13 12:23:51 +0000

    replace different thresholds by scaling the loss

--- config.py
+++ config.py
@@ -9,15 +9,16 @@ import torch
 @dataclass
 class Config:
     load_weights: bool = True
-    load_weights_from: str = "./weights/weights-22000.pth"
+    load_weights_from: str = "./weights/weights-pre-fine-23000.pth"
 
     load_pretrain_weights: bool = False
     load_pretrain_weights_from: str = "./weights/pretrain_weights-350000.pth"
 
     device: str = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     #                           Anger     Fear      Joy      Sadness   Surprise
-    label_set_thresholds = [ 0.160263, 0.129352, 0.357197, 0.313231, 0.046307]
-    #label_set_thresholds = [0.5]*5
+    #label_set_thresholds = [ 0.160263, 0.129352, 0.357197, 0.313231, 0.046307]
+    #label_set_thresholds = [0.3]*5
+    label_set_thresholds = [0.5]*5
 
     model_config = ModelConfig(
         num_classes = 5,
@@ -29,12 +30,12 @@ class Config:
     finetune_config = TrainingConfig(
         device = device,
         learning_rate = 0.0001,
-        num_epochs = 20,
+        num_epochs = 10,
         log_interval = 100,           # Log training loss every log_interval steps
         eval_interval = 1000,         # Evaluate the model every eval_interval steps
         save_interval = 1000,         # Save weights every save_interval steps
-        save_weights = False,
-        save_weights_to = "./weights/weights-<training_step>.pth", # <training_step> placeholder will be replaced
+        save_weights = True,
+        save_weights_to = "./weights/weights-pre-fine2-<training_step>.pth", # <training_step> placeholder will be replaced
     )
     pretraining_config = TrainingConfig(
         device = device,
@@ -45,7 +46,7 @@ class Config:
         save_interval = 10000,         # Save weights every save_interval steps
         save_weights = False,
         save_weights_to = "./weights/pretrain_weights-<training_step>.pth", # <training_step> placeholder will be replaced
-        unfreeze_count = 2
+        unfreeze_count = 2 #model_config.num_encoder_layers
     )
     data_config = DataConfig(
         finetuning_h5_path = "data/finetuning_split.h5",
